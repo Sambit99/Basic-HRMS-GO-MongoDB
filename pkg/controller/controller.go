9c324621ac1d8576e9d37cb38a2c8dfd1de3b363
Sambit99 <sambitkumar75@gmail.com>
2025-09-15 22:02:36 +0530

    feat: Implement employee controller with CRUD function stubs

---
+++ pkg/controller/controller.go
@@ -0,0 +1,23 @@
+package controller
+
+import "github.com/gofiber/fiber/v2"
+
+func GetAllEmployee(c *fiber.Ctx) error {
+	return nil
+}
+
+func GetEmployee(c *fiber.Ctx) error {
+	return nil
+}
+
+func NewEmployee(c *fiber.Ctx) error {
+	return nil
+}
+
+func DeleteEmployee(c *fiber.Ctx) error {
+	return nil
+}
+
+func UpdateEmployee(c *fiber.Ctx) error {
+	return nil
+}
