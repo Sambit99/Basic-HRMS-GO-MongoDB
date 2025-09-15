371f07eeb1bcb900e5aae96d6d72dddd6b587bbe
Sambit99 <sambitkumar75@gmail.com>
2025-09-15 21:48:15 +0530

    feat: Add Employee model with MongoDB BSON tags

---
+++ pkg/model/employee.go
@@ -0,0 +1,10 @@
+package model
+
+import "go.mongodb.org/mongo-driver/v2/bson"
+
+type Employee struct {
+	ID     bson.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name   string        `json:"name" bson:"name"`
+	Salary float64       `json:"salary" bson:"salary"`
+	Age    int64         `json:"age,omitempty" bson:"age,omitempty"`
+}
