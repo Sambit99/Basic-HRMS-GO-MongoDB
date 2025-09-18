7dd4d9e37fff623bc0320c8f10c0d41d3d29fb82
Sambit99 <sambitkumar75@gmail.com>
2025-09-18 20:09:10 +0530

    feat: Implement GetAllEmployee function to retrieve and return all employee records

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,9 +1,29 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/Sambit99/Basic-HRMS-GO-MongoDB/pkg/service"
+	"github.com/gofiber/fiber/v2"
+)
 
 func GetAllEmployee(c *fiber.Ctx) error {
-	return nil
+	employees := service.GetEmployees()
+
+	c.Set("Content-Type", "application/json")
+
+	if len(employees) == 0 {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"status":     "success",
+			"statuscode": 200,
+			"message":    "No records found",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":     "success",
+		"statuscode": 200,
+		"message":    "All records fetched successfully",
+		"data":       employees,
+	})
 }
 
 func GetEmployee(c *fiber.Ctx) error {
